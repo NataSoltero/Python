b89649f58f42f048eac53ae8f3c5315c7c1acd5f
NataSoltero <josue.soltero@gmail.com>
2024-07-30 19:44:13 -0600

    dia 2

---
+++ Dia 2/IntegersAndFloats.py
@@ -0,0 +1,20 @@
+my_number = 1
+print(my_number)
+
+print(type(my_number))
+
+my_operation = 1 + 3
+print(my_operation)
+
+my_float = 5.8
+print(my_float)
+print(type(my_float))
+
+my_float = my_float + my_float
+print(my_float)
+
+age = input("Tell me your age: ")
+print("Your age is: " + age)
+
+new_age = 1 + age
+print("You are turning: " + new_age)
---
+++ Dia 2/Variables.py
@@ -0,0 +1,19 @@
+name = "Juan"
+print(name)
+
+name = "Laura"
+print(name)
+
+age = 30
+print(age)
+
+age2 = 15
+print(age + age2)
+
+newname = input("Tell me your name: ")
+print("Your name is: " + newname)
+
+language = "Python"
+phrase = newname + " is learning " + language
+print(phrase)
+
